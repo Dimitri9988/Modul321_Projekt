d0a5664e3277dad76e3b2e301187d26c0744451c
Dimitri9988 <dimitri.steiner@ict.csbe.ch>
2023-11-16 22:59:52 +0100

    Dockerfile Erstellt

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,25 @@
 # Use the official Node.js image as the base image
-FROM node:18
+FROM node:latest
 
 # Set the working directory inside the container
-WORKDIR ...
+WORKDIR /321-project-starter
 
 # Copy the package.json and package-lock.json files to the container
-COPY ...
+COPY package.json .
+COPY package-lock.json .
 
 # Install the dependencies
-RUN ...
+RUN npm install
+
 
 # Copy the source code to the container
-COPY ...
+COPY app.js .
+COPY server/api.js server/
+COPY server/database.js server/
+COPY server/websocketserver.js server/
+COPY client/index.html client/
+COPY client/index.js client/
+COPY client/style.css client/
 
 # Start the server when the container starts
-CMD ...
+CMD ["node", "app.js"]
--- server/api.js
+++ server/api.js
@@ -5,7 +5,6 @@ const initializeAPI = (app) => {
   // default REST api endpoint
   app.get("/api/hello", hello);
   //app.post("/api/sendChat", receiveChat);
-  app.post("/api/sendUser", receiveUser);
 };
 
 
@@ -15,50 +14,4 @@ const hello = (req, res) => {
 };
 
 
-
-
-
-
-
-/*
-const receiveChat = async (req, res) => {
-  const { message, username, timeStamp } = req.body;
-  const userIdResult = await executeSQL(`SELECT id FROM users WHERE name = '${username}'`);
-  if (userIdResult && userIdResult.length > 0) {
-    userId = userIdResult[0].id;
-  } else {
-    userId = 17;
-  }
-  if (message === "smile") {
-    const newMessage = "(＾◡＾)"
-    const query = `INSERT INTO messages (user_id, message, timestamp) VALUES (${userId}, "${newMessage}", "${timeStamp}")`;
-    await executeSQL(query);
-  }
-  else if (message === "wizard") {
-    const newMessage = "(∩^o^)⊃━☆"
-    const query = `INSERT INTO messages (user_id, message, timestamp) VALUES (${userId}, "${newMessage}", "${timeStamp}")`;
-    await executeSQL(query);
-  }
-  else if (message === "") {
-    const newMessage = ""
-    const query = `INSERT INTO messages (user_id, message, timestamp) VALUES (${userId}, "${newMessage}", "${timeStamp}")`;
-    await executeSQL(query);
-  }
-  else {
-    const query = `INSERT INTO messages (user_id, message, timestamp) VALUES (${userId}, "${message}", "${timeStamp}")`;
-    await executeSQL(query);
-  }
-
-} */
-
-
-const receiveUser = async (req, res) => {
-  const { username} = req.body;
-  console.log(username)
-
-  const query = `INSERT INTO users (name) VALUES ("${username}")`;
-
-  await executeSQL(query);
-
-}
 module.exports = { initializeAPI };
